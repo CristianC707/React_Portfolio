a8b4be4b09c3a2ed2533a8280a03e20eeac5d690
CristianC707 <cristian.e.cuevas707@gmail.com>
2021-11-17 20:30:29 -0500

    Added to projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,22 +1,4 @@
 export const projects = [
-  // {
-  //   title: 'MERN Memories',
-  //   description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
-  //     image: '/images/1.png',
-  //     tags: ['Mongo', 'Express', 'React', 'Node'],
-  //   source: 'https://google.com',
-  //   visit: 'https://google.com',
-  //   id: 0,
-  // },
-  // {
-  //   title: 'E-Commerce',
-  //   description:"While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
-  //   image: '/images/2.png',
-  //   tags: ['React', 'JavaScript'],
-  //   source: 'https://google.com',
-  //   visit: 'https://google.com',
-  //   id: 1,
-  // },
   {
     title: 'Budget Tracker',
     description: "This is a code repository for the corresponding application. In this app, we're able to keep track of our finances.",
@@ -24,7 +6,7 @@ export const projects = [
       tags: ['Express', 'Mongoose', 'Lite-server', 'Morgan'],
     source: 'https://serene-plains-11489.herokuapp.com/',
     visit: 'https://github.com/CristianC707/budget_tracker',
-    id: 2,
+    id: 0,
   },
   {
     title: 'Note Taker',
@@ -33,7 +15,25 @@ export const projects = [
     tags: ['Express', 'Uuidv4', 'Fs', 'Path'],
     source: 'https://damp-spire-00197.herokuapp.com/',
     visit: 'https://github.com/CristianC707/note_taker',
-    id: 3,
+    id: 1,
+  },
+  {
+    title: 'Memories',
+    description: "IN PROGRESS: This is a code repository for the corresponding application. I created a social media app that allows the user to create and delete posts, login and have the ability to comment.",
+    image: '/images/1.png',
+    tags: ['MongoDB', 'Express', 'React', 'Node'],
+    source: 'https://github.com/CristianC707/MERN-Memories',
+    visit: 'https://github.com/CristianC707/MERN-Memories',
+    id: 2,
+  },
+  {
+    title: 'COMING SOON',
+    description: [ 'Memories: Due Date 11-22-21', <br />, 'E-Commerce Store', <br />, 'Quiz App' , <br />, 'Realtime Chat Messenger', <br />, 'Cryptocurrency App'],
+    image: '/images/2.png',
+    tags: ['N/A'],
+    source: 'https://github.com/CristianC707',
+    visit: 'https://github.com/CristianC707',
+    id: 2,
   },
 ];
 
